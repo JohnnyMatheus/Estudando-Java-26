507f5ed270d4dc301f2c598d4b88271956a913ee
Johnny Matheus <alunotioficial@gmail.com>
2026-04-05 23:06:14 -0300

    estudando

--- estrutura_condicional/src/estrutura_condicional/ex2.java
+++ estrutura_condicional/src/estrutura_condicional/ex2.java
@@ -4,7 +4,9 @@ public class ex2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		//Fazer um programa para ler um número inteiro e dizer se este número é par ou ímpar
+		
+		
 	}
 
 }
