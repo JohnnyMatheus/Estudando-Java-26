01ba05b6233c8087f866aa1688a0bb3e3f708c3e
Johnny Matheus <alunotioficial@gmail.com>
2026-04-05 23:05:57 -0300

    estudando

--- estrutura_condicional/src/estrutura_condicional/ex1.java
+++ estrutura_condicional/src/estrutura_condicional/ex1.java
@@ -1,9 +1,25 @@
 package estrutura_condicional;
-
+import java.util.Scanner;
 public class ex1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//Fazer um programa para ler um número inteiro, e depois dizer se este número é negativo ou não.
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Informe um número: ");
+		int num = sc.nextInt();
+		
+		if(num == 0 ) {
+			System.out.println("NEUTRO");
+		}else if (num >0) {
+			System.out.println("POSITIVO");
+
+		}else {
+			System.out.println("NEGATIVO");
+		}
+		
 		
 	}
 
